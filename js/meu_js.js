d8e220c59b5f63163a26859db100c140501341ae
vitorlorente <lorente.sociologia@gmail.com>
2019-03-10 23:15:04 -0300

    Commit inicial já com dois botões/abas de seleção de página, mais um botão para adicionar itens às páginas.

---
+++ js/meu_js.js
@@ -0,0 +1,68 @@
+function ativarGreen(){
+    $("#blue-container").addClass("escolha-inativo");
+    $("#blue-container").removeClass("escolha-ativo");
+    $("#btn-green").removeClass("btn-green-inativo");
+    $("#btn-green").addClass("btn-green-ativo");
+    $("#btn-blue").addClass("btn-blue-inativo");
+    $("#btn-blue").removeClass("btn-blue-ativo");
+    $("#green-container").addClass("escolha-ativo");
+    $("#green-container").removeClass("escolha-inativo");
+    $("#btn-add-quadrado").addClass("btn-add-green");
+    $("#btn-add-quadrado").removeClass("btn-add-blue");
+}
+
+function ativarBlue(){
+    $("#green-container").addClass("escolha-inativo");
+    $("#green-container").removeClass("escolha-ativo");
+    $("#btn-blue").addClass("btn-blue-ativo");
+    $("#btn-blue").removeClass("btn-blue-inativo");
+    $("#btn-green").removeClass("btn-green-ativo");
+    $("#btn-green").addClass("btn-green-inativo");
+    $("#blue-container").addClass("escolha-ativo");
+    $("#blue-container").removeClass("escolha-inativo");
+    $("#btn-add-quadrado").addClass("btn-add-blue");
+    $("#btn-add-quadrado").removeClass("btn-add-green");
+
+}
+
+function addGreen(){
+    $("#container-quadrados-green").append(
+        "<div style='margin: 10px; height: 200px; width: 200px; background-color: green; float: left;'></div>"
+    );
+}
+
+function addBlue(){
+    $("#container-quadrados-blue").append(
+        "<div style='margin: 10px; height: 200px; width: 200px; background-color: blue; float: left;'></div>"
+    );
+}
+
+$(document).ready(function(){
+    ativarGreen();
+});
+
+$("#btn-green").click(function(){
+    if ($(this).hasClass("btn-green-ativo")){
+        console.log("Já ativo");
+    }
+    else {
+        ativarGreen();
+    }
+});
+
+$("#btn-blue").click(function(){
+    if ($(this).hasClass("btn-blue-ativo")){
+        console.log("Já ativo");
+    }
+    else {
+        ativarBlue();
+    }
+});
+
+$("#btn-add-quadrado-green").click(function(){
+    addGreen();
+});
+
+$("#btn-add-quadrado-blue").click(function(){
+    addBlue();
+});
